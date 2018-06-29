3f2cfa0112b262f9cfba667141a0006ec1b8292b
Dan Toye <dan.toye@clixxa.com>
2018-06-29 00:24:06 -0700

    more loggin in deploy script

--- cmd/deploy.master.sh
+++ cmd/deploy.master.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "building..."
 cd $GIT_WORK_TREE
 pwd
+echo "fetching depedencies..."
 go get ./...
+echo "building binary..."
 go build -o tb cmd/tinybiome.go
+echo "reloading nginx"
 service nginx reload
