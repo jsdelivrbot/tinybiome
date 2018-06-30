5e46b9682c797c3e12b83a72d193cd3f9abdbe03
Dan Toye <dan.toye@clixxa.com>
2018-06-30 16:53:29 -0700

    strip caching headers

--- config.go
+++ config.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type RoomConfig struct {
@@ -105,7 +106,7 @@ type FilesConfig struct {
 func (fc FilesConfig) Run(c Config) error {
 	log.Println("serving files")
 	w := http.NewServeMux()
-	fs := http.FileServer(http.Dir(fc.Directory))
+	fs := NoCache(http.FileServer(http.Dir(fc.Directory)))
 	w.Handle("/", fs)
 
 	if fc.Insecure {
@@ -172,4 +173,42 @@ func (c Config) RunAndWait() error {
 	}
 
 	return rt.Wait()
+}
+
+var epoch = time.Unix(0, 0).Format(time.RFC1123)
+
+var noCacheHeaders = map[string]string{
+	"Expires":         epoch,
+	"Cache-Control":   "no-cache, private, max-age=0",
+	"Pragma":          "no-cache",
+	"X-Accel-Expires": "0",
+}
+
+var etagHeaders = []string{
+	"ETag",
+	"If-Modified-Since",
+	"If-Match",
+	"If-None-Match",
+	"If-Range",
+	"If-Unmodified-Since",
+}
+
+func NoCache(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		// Delete any ETag headers that may have been set
+		for _, v := range etagHeaders {
+			if r.Header.Get(v) != "" {
+				r.Header.Del(v)
+			}
+		}
+
+		// Set our NoCache headers
+		for k, v := range noCacheHeaders {
+			w.Header().Set(k, v)
+		}
+
+		h.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
 }
